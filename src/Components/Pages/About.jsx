3481601daf06738c2b646e0c6866a16086ff8a38
Mahsa <mahsa.ghezel.1995@gmail.com>
2024-04-09 10:36:47 +0330

    Updated the Resume.jsx

--- src/Components/Pages/About.jsx
+++ src/Components/Pages/About.jsx
@@ -1,40 +1,54 @@
 import React from 'react';
+import aboutImg from '../../asset/about.svg';
 import Resume from './Resume';
+import { Link } from 'react-router-dom';
 
 export default function About() {
   return (
     <div className='container'>
+      <div className='imageContainer'>
+        <h6 className='welcome-msg' role='alert'>
+          Hi there, So nice to see you in my page.
+        </h6>
+        <img src={aboutImg} alt='about img' className='about-img' />
+      </div>
       <div className='about'>
         <h1>
           About <span className='span'>Me</span>
         </h1>
         <p className='about-p'>
-          Hi, I'm Mahsa Ghezel, a 27-year-old software engineer and frontend
-          developer. I'm passionate about frontend development and aspire to
-          become a professional in this field. With my strong foundation in
-          programming and design, I'm eager to start my career in frontend
-          development and bring my skills to the table. I'm excited about
-          opportunities to work with dynamic teams and contribute to impactful
-          projects. Thank you for considering me for a frontend developer
-          position.
+          I am <span className='qoute'>Mahsa Ghezel</span>, a 28-year-old{' '}
+          <span className='qoute'>software engineer</span> and{' '}
+          <span className='qoute'>frontend developer</span>. I'm passionate
+          about frontend development and aspire to become a professional in this
+          field. With my strong foundation in programming and design, I'm eager
+          to start my career in frontend development and bring my skills to the
+          table. I'm excited about opportunities to work with dynamic teams and
+          contribute to impactful projects. Thank you for considering me for a
+          frontend developer position.
         </p>
         <h1>
           My <span className='span'>Resume</span>
         </h1>
         <p className='about-p'>
           I have expertise in a number of skills related to web development. I
-          am skilled in HTML, CSS, JavaScript, ReactJS, Bootstrap, responsive
-          web design, and Git. With these skills, I can design and develop
-          high-quality websites and web applications that are both user-friendly
-          and visually appealing. I am passionate about my work, and I am always
-          eager to learn new technologies and techniques to improve my skills. I
-          believe that my experience and skills make me a valuable asset to any
-          team or project in need of a skilled and dedicated web developer.
+          am skilled{' '}
+          <inspan className='qoute'>
+            {' '}
+            HTML, CSS, JavaScript, ReactJS, Bootstrap, responsive web
+            design,Redux.js, TypeScript and GitHub
+          </inspan>
+          . With these skills, I can design and develop high-quality websites
+          and web applications that are both user-friendly and visually
+          appealing. I am passionate about my work, and I am always eager to
+          learn new technologies and techniques to improve my skills. I believe
+          that my experience and skills make me a valuable asset to any team or
+          project in need of a skilled and dedicated web developer.
         </p>
         <Resume />
         <div className='text-center'>
           <div className='btn btn-outline-secondary mt-4 text-center'>
-            Hire Me
+            <Link to ='/contact' className='text-decoration-none'>Contact with me</Link>
           </div>
         </div>
       </div>
--- src/Components/Pages/Resume.jsx
+++ src/Components/Pages/Resume.jsx
@@ -1,99 +1,109 @@
+import logoTechniq from '../../asset/logo_Technical-and-Vocational-University-TVU.jpg';
+import meta from '../../asset/certificates/C67N7RKEJ7DK.jpeg';
+import reactCer from '../../asset/certificates/169865.png'
+import responsive from '../../asset/certificates/168338.png'
+import web from '../../asset/certificates/164825.png'
+import coding from '../../asset/certificates/162038.png'
+import { Link } from 'react-router-dom';
 export default function Resume() {
   return (
     <>
-      <div className="resume row justify-content-center">
-        <div className="f-sect col-12 col-lg-6">
-          <h1 className="mb-5">Education</h1>
-          <div className="education row align-items-center mb-5">
-            <div className="years col">
-              <div className="year">
-                <h4>2021</h4>
-                <h5>Maedeh University</h5>
-              </div>
-              <div className="year">
-                <h4>2017</h4>
-                <h5>Maedeh University</h5>
-              </div>
-            </div>
-            <div className="vl col"></div>
-            <div className="description col">
-              <div className="desc">
-                <h3>Software Engineering</h3>
-              </div>
-              <div className="desc">
-                <h3>Software Associate license</h3>
-              </div>
+      <div className='resume col justify-content-center'>
+        <h1 className='h1-about'>Educations</h1>
+        <div className='educations'>
+          <div className='sections'>
+            <img src={logoTechniq} alt='' className='logoImg' />
+            <div className='degrees'>
+              <h3>Software Engineering</h3>
+              <h6>Technical and Vocational University</h6>
             </div>
           </div>
-          <h1 className="mb-5">Experience</h1>
-          <div className="experience  row align-items-center">
-            <div className="years col">
-              <div className="year">
-                <h4>2022 - Current</h4>
-                <h5>Freelance</h5>
-              </div>
-              <div className="year">
-                <h4>2021</h4>
-                <h5>ITRC</h5>
-              </div>
-            </div>
-            <div className="vl col"></div>
-            <div className="description col">
-              <div className="desc">
-                <h3>Front-End Developer</h3>
-              </div>
-              <div className="desc">
-                <h3>Junior Software Engineer</h3>
-              </div>
+          <div className='sections'>
+            <img src={logoTechniq} alt='' className='logoImg' />
+            <div className='degrees'>
+              <h3>Software Associate Degree</h3>
+              <h6>Technical and Vocational University</h6>
             </div>
           </div>
         </div>
-        <div className="sec-sect col-12 col-lg-6">
-          <div className="coding-skills">
-            <div className="progress-container">
-              <div className="my-label">
-                <h3>HTML/CSS</h3>
-                <h5>95%</h5>
-              </div>
-              <div className="progress">
-                <div className="inside"></div>
-              </div>
+        <h1 className='h1-about'>Certificates</h1>
+        <div className='certificates'>
+          <div className='certificate-sections'>
+            <img src={meta} alt='' className='certificateImg' />
+            <div className='meta-detail'>
+              <h3>Advanced React</h3>
+              <h6>Meta / Coursera</h6>
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo
+                quod, cum omnis incidunt nihil sed recusandae ab sapiente
+                doloribus aspernatur.
+              </p>
+              <Link to='https://coursera.org/share/d7b1009ecdb3db7421f52ad7b197af74'>
+                Download the certificate
+              </Link>
             </div>
-            <div className="progress-container">
-              <div className="my-label">
-                <h3>JavaScript</h3>
-                <h5>70%</h5>
-              </div>
-              <div className="progress">
-                <div className="inside-70"></div>
-              </div>
+          </div>
+          <div className='certificate-sections'>
+            <div className='meta-detail'>
+              <h3>React Development</h3>
+              <h6>SheCodes</h6>
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo
+                quod, cum omnis incidunt nihil sed recusandae ab sapiente
+                doloribus aspernatur.
+              </p>
+              <Link to='https://s3.amazonaws.com/shecodesio-production/students/certificates/000/169/865/original/169865.png?1691405553'>
+                Download the certificate
+              </Link>
             </div>
-            <div className="progress-container">
-              <div className="my-label">
-                <h3>React</h3>
-                <h5>60%</h5>
-              </div>
-              <div className="progress">
-                <div className="inside-50"></div>
-              </div>
+            <img src={reactCer} alt='' className='certificateImg' />
+          </div>
+          <div className='certificate-sections'>
+            <img src={responsive} alt='' className='certificateImg' />
+            <div className='meta-detail'>
+              <h3>Responsive Web Development</h3>
+              <h6>SheCodes</h6>
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo
+                quod, cum omnis incidunt nihil sed recusandae ab sapiente
+                doloribus aspernatur.
+              </p>
+              <Link to='https://coursera.org/share/d7b1009ecdb3db7421f52ad7b197af74'>
+                Download the certificate
+              </Link>
             </div>
-            <div className="progress-container">
-              <div className="my-label">
-                <h3>Responsive WebDesign</h3>
-                <h5>95%</h5>
-              </div>
-              <div className="progress">
-                <div className="inside"></div>
-              </div>
+          </div>
+          <div className='certificate-sections'>
+            <div className='meta-detail'>
+              <h3>Web Development</h3>
+              <h6>SheCodes</h6>
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo
+                quod, cum omnis incidunt nihil sed recusandae ab sapiente
+                doloribus aspernatur.
+              </p>
+              <Link to='https://s3.amazonaws.com/shecodesio-production/students/certificates/000/164/825/original/164825.png?1688902262'>
+                Download the certificate
+              </Link>
             </div>
-            <div className="progress-container">
-              <div className="my-label">
-                <h3>Bootstrap</h3>
-                <h5>70%</h5>
-              </div>
-              <div className="progress">
-                <div className="inside-70"></div>
-              </div>
+            <img src={web} alt='' className='certificateImg' />
+          </div>
+          <div className='certificate-sections'>
+            <img src={coding} alt='' className='certificateImg' />
+            <div className='meta-detail'>
+              <h3>Introduction to Coding</h3>
+              <h6>SheCodes</h6>
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo
+                quod, cum omnis incidunt nihil sed recusandae ab sapiente
+                doloribus aspernatur.
+              </p>
+              <Link
+                to='https://coursera.org/share/d7b1009ecdb3db7421f52ad7b197af74'
+                className='text-decoration-none'
+              >
+                Download the certificate
+              </Link>
             </div>
           </div>
         </div>
