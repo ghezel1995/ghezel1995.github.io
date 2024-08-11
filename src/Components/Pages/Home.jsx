15a48d725f2fbce868607de3835b23abba06c813
Mahsa <mahsa.ghezel.1995@gmail.com>
2024-08-11 08:57:45 +0330

    Made Home.jsx responsive

--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -4,10 +4,10 @@ import './style.css';
 
 export default function Home() {
   return (
-    <div className='container row'>
+    <div className='container-sm row'>
       <div className='col-12 col-lg-6  home-body'>
         <h1 className='my-name'>Mahsa Ghezel</h1>
-        <h6 className='my-job'>Front-End Developer / Software Engineer</h6>
+        <h6 className='my-job'>Front-End Developer | Software Engineer</h6>
         <div className='me-icons'>
           <a
             href='https://www.linkedin.com/in/mahsaghezel/'
@@ -24,10 +24,16 @@ export default function Home() {
             <i className='bi bi-github s-icons'></i>
           </a>
         </div>
-        <a href='./Contact' className='btn btn-outline-secondary' >Contact Me</a>
+        <a href='./Contact' className='btn btn-outline-secondary'>
+          Contact Me
+        </a>
       </div>
       <div className='col-12 col-lg-6  home-body'>
-        <img src={me} className='w-50 rounded my-img' alt='' />
+        <img
+          src={me}
+          className='w-50 rounded my-img d-none d-md-block'
+          alt=''
+        />
       </div>
     </div>
   );
